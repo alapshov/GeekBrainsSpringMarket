0d5370ef944a934101c321947f155cbf68631fb0
Алексей <avlapshov@dasreda.ru>
2022-06-03 19:28:11 +0300

    Task_8

---
+++ market/api/src/main/java/ru/geekbrains/march/market/api/DataEnum.java
@@ -0,0 +1,5 @@
+package ru.geekbrains.march.market.api;
+
+public enum DataEnum {
+    A, B, C
+}
--- market/core-service/src/main/java/ru/geekbrains/march/market/core/controllers/ProductController.java
+++ market/core-service/src/main/java/ru/geekbrains/march/market/core/controllers/ProductController.java
@@ -1,14 +1,20 @@
 package ru.geekbrains.march.market.core.controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-import ru.geekbrains.march.market.api.PageDto;
 import ru.geekbrains.march.market.api.ProductDto;
 import ru.geekbrains.march.market.core.converters.ProductConverter;
 import ru.geekbrains.march.market.core.entities.Product;
+import ru.geekbrains.march.market.core.exceptions.AppError;
 import ru.geekbrains.march.market.core.exceptions.ResourceNotFoundException;
 import ru.geekbrains.march.market.core.repositories.specifications.ProductsSpecifications;
 import ru.geekbrains.march.market.core.services.ProductService;
@@ -20,17 +26,18 @@ import java.util.stream.Collectors;
 @RestController
 @RequestMapping("/api/v1/products")
 @RequiredArgsConstructor
+@Tag(name = "Продукты", description = "Методы работы с продуктами")
 public class ProductController {
     private final ProductService productService;
     private final ProductConverter productConverter;
 
     @GetMapping
     public Page<ProductDto> getAllProducts(
-            @RequestParam(name = "p", defaultValue = "1") Integer page,
-            @RequestParam(name = "page_size", defaultValue = "10") Integer pageSize,
-            @RequestParam(name = "title_part", required = false) String titlePart,
-            @RequestParam(name = "min_price", required = false) Integer minPrice,
-            @RequestParam(name = "max_price", required = false) Integer maxPrice
+            @RequestParam(name = "p", defaultValue = "1") @Parameter(description = "Номер страницы", required = true) Integer page,
+            @RequestParam(name = "page_size", defaultValue = "10") @Parameter(description = "Номер страницы", required = false)  Integer pageSize,
+            @RequestParam(name = "title_part", required = false) @Parameter(description = "Фильтр части названия продукта", required = false)  String titlePart,
+            @RequestParam(name = "min_price", required = false) @Parameter(description = "Фильтр по мин цене продукта", required = false)  Integer minPrice,
+            @RequestParam(name = "max_price", required = false) @Parameter(description = "Фильтр по макс цене продукта", required = false)  Integer maxPrice
     ) {
         if (page < 1) {
             page = 1;
@@ -48,11 +55,32 @@ public class ProductController {
         return productService.findAll(page - 1, pageSize, spec).map(productConverter::entityToDto);
     }
 
+    @Operation(
+            summary = "Запрос на получение продукта по id",
+            responses = {
+                    @ApiResponse(
+                            description = "Успешный ответ", responseCode = "200",
+                            content = @Content(schema = @Schema(implementation = ProductDto.class))
+                    ),
+                    @ApiResponse(
+                            description = "Продукт не найден", responseCode = "404",
+                            content = @Content(schema = @Schema(implementation = AppError.class))
+                    )
+            }
+    )
     @GetMapping("/{id}")
-    public ProductDto getProductById(@PathVariable Long id) {
+    public ProductDto getProductById(@PathVariable @Parameter(description = "Идентификатор продукта", required = true) Long id) {
         return productConverter.entityToDto(productService.findById(id).orElseThrow(() -> new ResourceNotFoundException("Продукт с id: " + id + " не найден")));
     }
 
+    @Operation(
+            summary = "Запрос на создание нового продукта",
+            responses = {
+                    @ApiResponse(
+                            description = "Продукт успешно создан", responseCode = "201"
+                    )
+            }
+    )
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public void createNewProducts(@RequestBody ProductDto productDto) {
